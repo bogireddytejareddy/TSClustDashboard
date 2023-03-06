a0e11ea30a3dcffb28fe6a1f56648999cbc72df2
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2023-03-05 18:33:22 -0600

    Create statistical_test.py

---
+++ statistical_test.py
@@ -0,0 +1,219 @@
+import os
+import numpy as np
+import pandas as pd
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['font.sans-serif'] = 'Arial'
+
+import operator
+import math
+from scipy.stats import wilcoxon
+from scipy.stats import friedmanchisquare
+import networkx
+
+
+def graph_ranks(avranks, names, cd=None, cdmethod=None, lowv=None, highv=None,
+                width=6, textspace=1, reverse=False, filename=None, **kwargs):
+    try:
+        import matplotlib.pyplot as plt
+        from matplotlib.backends.backend_agg import FigureCanvasAgg
+    except ImportError:
+        raise ImportError("Function graph_ranks requires matplotlib.")
+
+    width = float(width)
+    textspace = float(textspace)
+
+    def nth(l, n):
+        n = lloc(l, n)
+        return [a[n] for a in l]
+
+    def lloc(l, n):
+        if n < 0:
+            return len(l[0]) + n
+        else:
+            return n
+
+    def mxrange(lr):
+        if not len(lr):
+            yield ()
+        else:
+            # it can work with single numbers
+            index = lr[0]
+            if isinstance(index, int):
+                index = [index]
+            for a in range(*index):
+                for b in mxrange(lr[1:]):
+                    yield tuple([a] + list(b))
+
+    def print_figure(fig, *args, **kwargs):
+        canvas = FigureCanvasAgg(fig)
+        canvas.print_figure(*args, **kwargs)
+
+    sums = avranks
+
+    tempsort = sorted([(a, i) for i, a in enumerate(sums)], reverse=reverse)
+    ssums = nth(tempsort, 0)
+    sortidx = nth(tempsort, 1)
+    nnames = [names[x] for x in sortidx]
+
+    if lowv is None:
+        lowv = min(1, int(math.floor(min(ssums))))
+    if highv is None:
+        highv = max(len(avranks), int(math.ceil(max(ssums))))
+
+    cline = 0.4
+
+    k = len(sums)
+
+    lines = None
+
+    linesblank = 0
+    scalewidth = width - 2 * textspace
+
+    def rankpos(rank):
+        if not reverse:
+            a = rank - lowv
+        else:
+            a = highv - rank
+        return textspace + scalewidth / (highv - lowv) * a
+
+    distanceh = 0.25
+
+    if cd and cdmethod is None:
+        # get pairs of non significant methods
+
+        def get_lines(sums, hsd):
+            # get all pairs
+            lsums = len(sums)
+            allpairs = [(i, j) for i, j in mxrange([[lsums], [lsums]]) if j > i]
+            # remove not significant
+            notSig = [(i, j) for i, j in allpairs
+                      if abs(sums[i] - sums[j]) <= hsd]
+            # keep only longest
+
+            def no_longer(ij_tuple, notSig):
+                i, j = ij_tuple
+                for i1, j1 in notSig:
+                    if (i1 <= i and j1 > j) or (i1 < i and j1 >= j):
+                        return False
+                return True
+
+            longest = [(i, j) for i, j in notSig if no_longer((i, j), notSig)]
+
+            return longest
+
+        lines = get_lines(ssums, cd)
+        linesblank = 0.2 + 0.2 + (len(lines) - 1) * 0.1
+
+        # add scale
+        distanceh = 0.25
+        cline += distanceh
+
+    # calculate height needed height of an image
+    minnotsignificant = max(2 * 0.2, linesblank)
+    height = cline + ((k + 1) / 2) * 0.2 + minnotsignificant
+
+    fig = plt.figure(figsize=(width, height))
+    fig.set_facecolor('white')
+    ax = fig.add_axes([0, 0, 1, 1])  # reverse y axis
+    ax.set_axis_off()
+
+    hf = 1. / height  # height factor
+    wf = 1. / width
+
+    def hfl(l):
+        return [a * hf for a in l]
+
+    def wfl(l):
+        return [a * wf for a in l]
+
+
+    # Upper left corner is (0,0).
+    ax.plot([0, 1], [0, 1], c="w")
+    ax.set_xlim(0, 1)
+    ax.set_ylim(1, 0)
+
+    def line(l, color='k', **kwargs):
+        """
+        Input is a list of pairs of points.
+        """
+        ax.plot(wfl(nth(l, 0)), hfl(nth(l, 1)), color=color, **kwargs)
+
+    def text(x, y, s, *args, **kwargs):
+        ax.text(wf * x, hf * y, s, size=18, *args, **kwargs)
+
+    line([(textspace, cline), (width - textspace, cline)], linewidth=1.7)
+
+    bigtick = 0.1
+    smalltick = 0.05
+
+    tick = None
+    for a in list(np.arange(lowv, highv, 0.5)) + [highv]:
+        tick = smalltick
+        if a == int(a):
+            tick = bigtick
+        line([(rankpos(a), cline - tick / 2),
+              (rankpos(a), cline)],
+             linewidth=1.7)
+
+    for a in range(lowv, highv + 1):
+        text(rankpos(a), cline - tick / 2 - 0.05, str(a),
+             ha="center", va="bottom")
+
+    k = len(ssums)
+
+    a = 1
+    b = a + 0.1
+
+    for i in range(math.ceil(k / 2)):
+        chei = cline + minnotsignificant + i * 0.24
+        line([(rankpos(ssums[i]), cline),
+              (rankpos(ssums[i]), chei),
+              (textspace + b, chei)],
+             linewidth=1.7)
+        text(textspace + a, chei, nnames[i], ha="right", va="center")
+
+    for i in range(math.ceil(k / 2), k):
+        chei = cline + minnotsignificant + (k - i - 1) * 0.24
+        line([(rankpos(ssums[i]), cline),
+              (rankpos(ssums[i]), chei),
+              (textspace + scalewidth - b, chei)],
+             linewidth=1.7)
+        text(textspace + scalewidth - a, chei, nnames[i],
+             ha="left", va="center")
+
+    if cd and cdmethod is None:
+        # upper scale
+        if not reverse:
+            begin, end = rankpos(lowv), rankpos(lowv + cd)
+        else:
+            begin, end = rankpos(highv), rankpos(highv - cd)
+
+        # no-significance lines
+        def draw_lines(lines, side=0.05, height=0.1):
+            start = cline + 0.2
+            for l, r in lines:
+                line([(rankpos(ssums[l]) - side, start),
+                      (rankpos(ssums[r]) + side, start)],
+                     linewidth=2.5)
+                start += height
+
+        draw_lines(lines)
+
+    elif cd:
+        begin = rankpos(avranks[cdmethod] - cd)
+        end = rankpos(avranks[cdmethod] + cd)
+        line([(begin, cline), (end, cline)],
+             linewidth=2.5)
+        line([(begin, cline + bigtick / 2),
+              (begin, cline - bigtick / 2)],
+             linewidth=2.5)
+        line([(end, cline + bigtick / 2),
+              (end, cline - bigtick / 2)],
+             linewidth=2.5)
+
+    if filename:
+        print_figure(fig, filename, **kwargs)
