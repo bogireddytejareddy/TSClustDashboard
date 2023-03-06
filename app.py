1f2ef20f38dbf23182ab6a0d16c59906a76a43cf
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2023-03-06 13:05:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ characteristics_df.drop(columns=characteristics_df.columns[0], axis=1, inplace=T
 characteristics_df.columns = ['Name', 'NumOfTrainingSamples', 'NumOfTestingSample', 'NumOfSamples', 'SeqLength', 'NumOfClusters', 'Type']
 characteristics_df = characteristics_df[['Name', 'NumOfSamples', 'SeqLength', 'NumOfClusters', 'Type']]
 
-@st.cache_data
+
 def plot_stat_plot(df, metric_name, methods_family, datasets):
     container_method = st.container()
     stat_methods_family = container_method.multiselect('Select a group of methods', sorted(methods_family), key='selector_stat_methods')
@@ -88,7 +88,7 @@ def plot_time_plot(measure_name):
 
         st.plotly_chart(fig, theme="streamlit", use_container_width=False)
 
-@st.cache_data
+
 def plot_misconceptions_plot(metric_name, datasets):
     tab4, tab5, tab6 = st.tabs(["Distance Measures", "Supervised vs. Unsupervised Tuning", "Deep Learning Methods"])
     with tab4:
