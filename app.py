cf2ed1eb9178ebebf2ec37350cebebb83ec04a72
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2023-03-24 12:46:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def plot_stat_plot(df, metric_name, methods_family, datasets):
                 graph_ranks(avranks, names, cd=cd, width=9, textspace=1.25)
                 fig = plt.show()
                 st.pyplot(fig)
-                print(rank_df)
+                st.markdown(cd)
 
             stat_plots(df)
     
