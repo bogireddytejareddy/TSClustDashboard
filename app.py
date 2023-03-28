c38e0117d9b9cdbd85f66ec6d1cfe840277d5bde
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2023-03-27 21:45:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -830,7 +830,7 @@ def plot_ablation(df, metric_name, datasets):
 
                 stat_plots(df)
     with tab16:
-        df = df.loc[all_df['Dataset'].isin(datasets)][[method_g + '-' + metric_name for method_g in ['DCN', 'DEC', 'IDEC', 'DEPICT', 'DTC', 'DTCR', 'SDCN', 'SOM_VAE', 'ClusterGAN', 'VADE']]]
+        df = df.loc[df['Dataset'].isin(datasets)][[method_g + '-' + metric_name for method_g in ['DCN', 'DEC', 'IDEC', 'DEPICT', 'DTC', 'DTCR', 'SDCN', 'SOM_VAE', 'ClusterGAN', 'VADE']]]
         df = df[df.mean().sort_values().index]
         fig = go.Figure()
         for i, cols in enumerate(df.columns[:]):
