d7912351bf77611613c64987d0e936be17b6ac71
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2023-03-31 18:21:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -910,12 +910,12 @@ with tab_desc:
     st.markdown('# Odyssey Engine')
     st.markdown(description_intro1)
     background = Image.open('./data/summary.png')
-    col1, col2, col3 = st.columns([1.8, 5, 0.2])
-    col2.image(background, width=700, caption='Overview of Odyssey’s architecture.')
+    col1, col2, col3 = st.columns([1.2, 5, 0.2])
+    col2.image(background, width=900, caption='Overview of Odyssey’s architecture.')
     st.markdown(description_intro2)
     background = Image.open('./data/taxonomy.png')
-    col1, col2, col3 = st.columns([1.8, 5, 0.2])
-    col2.image(background, width=700, caption='Taxonomy of time-series clustering methods in Odyssey.')
+    col1, col2, col3 = st.columns([1.2, 5, 0.2])
+    col2.image(background, width=900, caption='Taxonomy of time-series clustering methods in Odyssey.')
 
 with tab_acc:
     st.markdown('# Evaluation')
